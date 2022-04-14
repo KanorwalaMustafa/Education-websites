4d7773227638909fe83a27033714b3b77e03e377
Mustafa <kanorwalamustafa999@gmail.com>
2022-04-14 14:20:02 +0530

    first commit

---
+++ JavaScript/main.js
@@ -0,0 +1,24 @@
+// change navbar styles on scroll
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
+})
+
+//Show/Hide faq amswer
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        //change icon
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className === 'fa-solid fa-plus') {
+            icon.className = "fa-solid fa-minus";  
+        }
+        else{
+            icon.className = "fa-solid fa-plus";
+        }
+    })
+})
